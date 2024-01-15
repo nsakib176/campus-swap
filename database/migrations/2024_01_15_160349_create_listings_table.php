4a9ab9964e8b30906e4c6bea6627e9b22dbe720b
Nazmus Sakib <58693047+nsakib176@users.noreply.github.com>
2024-01-15 22:15:14 +0600

    Model, migration, controller added. Migration code done.

---
+++ database/migrations/2024_01_15_160349_create_listings_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('listings', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('book_id');
+            $table->unsignedBigInteger('user_id');
+            $table->string('condition');
+            $table->decimal('price', 10, 2)->nullable();
+            $table->decimal('swap_value', 10, 2)->nullable();
+            $table->boolean('available');
+            $table->string('location')->nullable();
+            $table->timestamps();
+
+            $table->foreign('book_id')->references('id')->on('books')->onDelete('cascade');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('listings');
+    }
+};
